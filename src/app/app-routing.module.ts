4c117a28cc448dff81f0ce449ec88d0dead0dce0
codefirst2022 <codefirst2022@gmail.com>
2023-12-11 17:26:13 +0530

    Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,6 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegistrationComponent } from './pages/registration/registration.component';
-import { JobsComponent } from './pages/jobs/jobs.component';
-import { JobDetailsComponent } from './pages/job-details/job-details.component';
-import { CreateNewJobComponent } from './pages/create-new-job/create-new-job.component';
-import { JobListingComponent } from './pages/job-listing/job-listing.component';
-import { MyJobsComponent } from './pages/my-jobs/my-jobs.component';
+import { RouterModule, Routes } from '@angular/router'; 
+ 
 
 const routes: Routes = [
    
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { JobService } from './service/job.service';
+import { Component } from '@angular/core'; 
 
 @Component({
   selector: 'app-root',
